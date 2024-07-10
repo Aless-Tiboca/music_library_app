fd0c0b5555c5b4526f1e63cabda9382673487800
Aless-Tiboca <91499890+Aless-Tiboca@users.noreply.github.com>
2024-07-10 16:31:37 +0300

    Delete unnecessary file

--- backend/services/searchService.js
+++
@@ -1,9 +0,0 @@
-const AlbumService = require('./albumService');
-
-const SearchService = {
-    searchAlbums: async (query) => {
-        return await AlbumService.searchAlbumsByTitle(query);
-    }
-};
-
-module.exports = SearchService;
